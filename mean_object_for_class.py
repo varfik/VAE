cd607b2761d9a599dc93d9b13fd1f3f35bc799fe
varfik <arinavarf@yandex.ru>
2025-05-29 12:00:43 +0300

    try to see

--- mean_object_for_class.py
+++ mean_object_for_class.py
@@ -1,19 +1,21 @@
+import os
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 from tensorflow.keras.datasets import cifar10
 from tensorflow.keras.layers import Layer
 from tensorflow.keras.utils import register_keras_serializable
+import matplotlib
+matplotlib.use('Agg')  # Устанавливаем бэкенд
 import matplotlib.pyplot as plt
 
-# Определяем и регистрируем функцию sampling
+# Определяем кастомные компоненты
 @register_keras_serializable()
 def sampling(args):
     z_mean, z_log_var = args
     epsilon = tf.random.normal(shape=tf.shape(z_mean))
     return z_mean + tf.exp(0.5 * z_log_var) * epsilon
 
-# Определяем KLDivergenceLayer с регистрацией
 @register_keras_serializable()
 class KLDivergenceLayer(Layer):
     def call(self, inputs):
@@ -25,40 +27,37 @@ class KLDivergenceLayer(Layer):
         self.add_loss(tf.reduce_mean(kl_loss))
         return z_mean
 
-# Загрузка данных CIFAR-10
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
-x_train = x_train.astype('float32') / 255.0
-
-# Загрузка моделей с указанием всех custom_objects
+# Загрузка моделей
 custom_objects = {
     'KLDivergenceLayer': KLDivergenceLayer,
     'sampling': sampling
 }
 
 encoder = load_model("encoder_final.h5", custom_objects=custom_objects)
+decoder = load_model("decoder_final.h5", custom_objects=custom_objects)
 
-# Список классов CIFAR-10
+# Обработка данных
+(x_train, y_train), _ = cifar10.load_data()
+x_train = x_train.astype('float32') / 255.0
 class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
-# Вычисляем средние латентные векторы
-class_latent_means = {}
-for class_id in range(10):
+# Создаем директорию для сохранения
+os.makedirs("vae_output", exist_ok=True)
+
+# Вычисляем и сохраняем средние изображения
+for class_id, class_name in enumerate(class_names):
+    # Получаем латентные векторы
     class_images = x_train[y_train.flatten() == class_id]
     latent_vectors = encoder.predict(class_images, batch_size=128)
     mean_latent = np.mean(latent_vectors, axis=0)
-    class_latent_means[class_names[class_id]] = mean_latent
-    print(f"Class: {class_names[class_id]}, Mean Latent Shape: {mean_latent.shape}")
-
-# Визуализация
-decoder = load_model("decoder_final.h5", custom_objects=custom_objects)
-
-plt.figure(figsize=(15, 8))
-for i, (class_name, mean_latent) in enumerate(class_latent_means.items()):
+    
+    # Генерируем и сохраняем изображение
     generated_image = decoder.predict(mean_latent.reshape(1, -1))[0]
-    plt.subplot(2, 5, i + 1)
+    plt.figure()
     plt.imshow(generated_image)
     plt.title(class_name)
     plt.axis('off')
-
-plt.tight_layout()
-plt.show()
+    plt.savefig(f"vae_output/{class_name}.png")
+    plt.close()
+    
+    print(f"Saved: vae_output/{class_name}.png")
