388cc538209eafb670424aa93af98aefec950e00
varfik <arinavarf@yandex.ru>
2025-05-29 11:58:35 +0300

    try to run

--- mean_object_for_class.py
+++ mean_object_for_class.py
@@ -3,9 +3,18 @@ import tensorflow as tf
 from tensorflow.keras.models import load_model
 from tensorflow.keras.datasets import cifar10
 from tensorflow.keras.layers import Layer
+from tensorflow.keras.utils import register_keras_serializable
 import matplotlib.pyplot as plt
 
-# Определяем KLDivergenceLayer (как в исходном коде)
+# Определяем и регистрируем функцию sampling
+@register_keras_serializable()
+def sampling(args):
+    z_mean, z_log_var = args
+    epsilon = tf.random.normal(shape=tf.shape(z_mean))
+    return z_mean + tf.exp(0.5 * z_log_var) * epsilon
+
+# Определяем KLDivergenceLayer с регистрацией
+@register_keras_serializable()
 class KLDivergenceLayer(Layer):
     def call(self, inputs):
         z_mean, z_log_var = inputs
@@ -16,17 +25,22 @@ class KLDivergenceLayer(Layer):
         self.add_loss(tf.reduce_mean(kl_loss))
         return z_mean
 
-# Загрузка данных CIFAR-10 (с метками классов)
+# Загрузка данных CIFAR-10
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
-x_train = x_train.astype('float32') / 255.0  # нормализация [0, 1]
+x_train = x_train.astype('float32') / 255.0
 
-# Загрузка encoder с указанием custom_objects
-encoder = load_model("encoder_final.h5", custom_objects={"KLDivergenceLayer": KLDivergenceLayer})
+# Загрузка моделей с указанием всех custom_objects
+custom_objects = {
+    'KLDivergenceLayer': KLDivergenceLayer,
+    'sampling': sampling
+}
+
+encoder = load_model("encoder_final.h5", custom_objects=custom_objects)
 
 # Список классов CIFAR-10
 class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
-# Вычисляем средние латентные векторы для каждого класса
+# Вычисляем средние латентные векторы
 class_latent_means = {}
 for class_id in range(10):
     class_images = x_train[y_train.flatten() == class_id]
@@ -35,8 +49,8 @@ for class_id in range(10):
     class_latent_means[class_names[class_id]] = mean_latent
     print(f"Class: {class_names[class_id]}, Mean Latent Shape: {mean_latent.shape}")
 
-# (Опционально) Визуализация средних изображений
-decoder = load_model("decoder_final.h5")  # decoder не требует custom_objects
+# Визуализация
+decoder = load_model("decoder_final.h5", custom_objects=custom_objects)
 
 plt.figure(figsize=(15, 8))
 for i, (class_name, mean_latent) in enumerate(class_latent_means.items()):
